76ff8f074f2aa790d6958e634760d28f1f02aafb
Iris Sun <irissun@live.com>
2023-02-04 22:06:23 -0500

    Step 9 - Set an Object Value - sum

--- js/ratings.js
+++ js/ratings.js
@@ -12,5 +12,6 @@ const collect_ratings = () => {
     elements.forEach(element => {
         rating = parseInt(element.id.replace('star', ''));
         ratings.count += parseInt(element.value);
+        ratings.sum += parseInt(element.value) * rating;
     });
 }
